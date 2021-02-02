19b65087beb7decbe35d66defd6fa25afc32d030
NekoChan <aaron74327@gmail.com>
2021-02-02 05:49:23 -0800

    update

--- src/app.js
+++ src/app.js
@@ -843,7 +843,7 @@ function file_video(path) {
 			<div class="mdui-container">
 				<div class="mdui-row-xs-2 mdui-m-b-1">
 					<div class="mdui-col">
-						${prev_child ? `<button id="leftBtn" onclick="${prev_child}" class="${btnClass1}">上一集</button>`
+						${prev_child ? `<button id="leftBtn" data-filepath="${prev_child}" class="${btnClass1}">上一集</button>`
 						: `<button class="${btnClass1}" disabled>上一集</button>`}
 					</div>
 					<div class="mdui-col">
